71a0ebe1d8b2e37eb53ad398a41aa0922d32aaa1
Javlonbek Khamzaev <85364717+JKhamzaev@users.noreply.github.com>
2025-01-09 20:02:52 -0500

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -2,7 +2,7 @@ export default function Head() {
   return (
     <>
       <title> Javlonbek Khamzaev </title>
-      <meta property="og:title" content="Javlonbek Khamzaev">
+      <meta property="og:title" content="Javlonbek Khamzaev"/>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Javlonbek Khamzaev" />
       <link
